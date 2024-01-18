c45874bbcd06b839113ddc7197df4562ec113d50
AdayGon <adaycg25@educastur.es>
2024-01-18 12:35:53 +0100

    Cambio de pantalla

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void ChangeLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void Salida()
+    {
+        Application.Quit(); //Salimos de la aplicación
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit(); //Salimos de la aplicación
+        }
+    }
+}
---
+++ Assets/Scripts/PulsarObjeto.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PulsarObjeto : MonoBehaviour
+{
+    public string sceneName;
+    public Canvas canvasSuperposicion;
+    public AudioSource audioSource;
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(sceneName);
+        audioSource.Play();
+    }
+
+    private void OnMouseOver()
+    {
+        canvasSuperposicion.gameObject.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        canvasSuperposicion.gameObject.SetActive(false);
+    }
+}
